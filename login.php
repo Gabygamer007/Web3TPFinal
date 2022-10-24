76fce70f61df91ea9b9532fc956939a6768e022d
Gabygamer007 <gabygamer0713@hotmail.com>
2022-10-24 16:02:19 -0400

    front-end login

---
+++ login.php
@@ -0,0 +1,24 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Page d'accueil</title>
+    <link href="css/login.css" rel="stylesheet" >
+</head>
+<body>
+    <div id="container">
+        <h1>Magix</h1>
+        <div id="box">
+            <form action="" method="post">
+                <div class="formbox">
+                    <input type="text" name="username" placeholder="Nom d'utilisateur" required>
+                    <input type="password" name="password" placeholder="Mot de passe" required>
+                    <button>Connexion</button>
+                </div>
+            </form>
+        </div>
+    </div>
+</body>
+</html>
