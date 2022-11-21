d2607a7e0f082317efc3cb081993f35354b14781
Gabygamer007 <gabygamer0713@hotmail.com>
2022-11-21 16:48:34 -0500

    game.php front end updated, almost finished

--- game.php
+++ game.php
@@ -24,23 +24,74 @@
             <div class="carte-cachee"></div>
         </div>
         <div class="opponent-info">
-            <div class="opponent-health"></div>
+            <div class="opponent-health"><div class="opponent-health-value">30</div></div>
             <div class="opponent-user-image"></div>
-            <div class="opponent-mp"></div>
+            <div class="opponent-mp"><div class=opponent-mp-value>0</div></div>
         </div>
-        <div class="opponent-card-left"></div>
+        <div class="opponent-card-left"><div class="opponent-card-left-value">25</div></div>
     </div>
     <div class="opponent-cards">
-        <div class="carte"></div>
-        <div class="carte"></div>
+        <div class="carte">
+            <div class="carte-cost">10</div>
+            <div class="carte-img" style="background-image: url(./img/image1-front-card.png); background-repeat: round; background-size: cover;"></div>
+            <div class="carte-name">Ninja</div>
+            <div class="carte-desc">Battlecry : Leech 1 HP from the opponent's hero</div>
+            <div class="carte-stats">
+                <div class="carte-health">10</div>
+                <div class="carte-atk">1</div>
+            </div>
+        </div>
+        <div class="carte">
+        <div class="carte-cost">8</div>
+            <div class="carte-img" style="background-image: url(./img/image2-front-card.png); background-repeat: round; background-size: cover;"></div>
+            <div class="carte-name">Ninja</div>
+            <div class="carte-desc">Stealth: At the start of your turn, give +1 HP to a random friendly minion</div>
+            <div class="carte-stats">
+                <div class="carte-health">1</div>
+                <div class="carte-atk">1</div>
+            </div>
+        </div>
         <div class="carte"></div>
         <div class="carte"></div>
         <div class="carte"></div>
         <div class="carte"></div>
         <div class="carte"></div>
     </div>
-    <div class="my-cards"></div>
-    <div class="my-bar"></div>
+    <div class="my-cards">
+        <div class="carte">
+            <div class="carte-cost">10</div>
+            <div class="carte-img" style="background-image: url(./img/image3-front-card.png); background-repeat: round; background-size: cover;"></div>
+            <div class="carte-name">Ninja</div>
+            <div class="carte-desc">Stealth: At the start of your turn, give +1 HP to a random friendly minion</div>
+            <div class="carte-stats">
+                <div class="carte-health">1</div>
+                <div class="carte-atk">1</div>
+            </div>
+        </div>
+        <div class="carte">
+            <div class="carte-cost">10</div>
+            <div class="carte-img" style="background-image: url(./img/image4-front-card.png); background-repeat: round; background-size: cover;"></div>
+            <div class="carte-name">Ninja</div>
+            <div class="carte-desc">Stealth: At the start of your turn, give +1 HP to a random friendly minion</div>
+            <div class="carte-stats">
+                <div class="carte-health">1</div>
+                <div class="carte-atk">1</div>
+            </div>
+        </div>
+        <div class="carte"></div>
+        <div class="carte"></div>
+        <div class="carte"></div>
+        <div class="carte"></div>
+        <div class="carte"></div></div>
+    <div class="my-bar">
+        <div class="my-info">
+            <div class="my-health"></div>
+            <div class="my-mp"></div>
+            <div class="my-cards-left"></div>
+        </div>
+        <div class="my-hand-cards"></div>
+        <div class="my-misc"></div>
+    </div>
     
 </body>
 <?php
