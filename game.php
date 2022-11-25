544204669e9b913fa20e404d6cab543774c0a18d
Gabygamer007 <gabygamer0713@hotmail.com>
2022-11-25 11:23:19 -0500

    end of game.php front end?

--- game.php
+++ game.php
@@ -24,9 +24,9 @@
             <div class="carte-cachee"></div>
         </div>
         <div class="opponent-info">
-            <div class="opponent-health"><div class="opponent-health-value">30</div></div>
+            <div class="opponent-health">30</div>
             <div class="opponent-user-image"></div>
-            <div class="opponent-mp"><div class=opponent-mp-value>0</div></div>
+            <div class="opponent-mp">0</div>
         </div>
         <div class="opponent-card-left"><div class="opponent-card-left-value">25</div></div>
     </div>
@@ -85,12 +85,27 @@
         <div class="carte"></div></div>
     <div class="my-bar">
         <div class="my-info">
-            <div class="my-health"></div>
-            <div class="my-mp"></div>
-            <div class="my-cards-left"></div>
+            <div class="my-health">30</div>
+            <div class="my-mp"><div class="my-mp-value">1</div></div>
+            <div class="my-cards-left"><div class="my-cards-left-value">25</div></div>
         </div>
-        <div class="my-hand-cards"></div>
-        <div class="my-misc"></div>
+        <div class="my-hand-cards">
+            <div class="carte">
+                <div class="carte-cost">10</div>
+                <div class="carte-img" style="background-image: url(./img/image4-front-card.png); background-repeat: round; background-size: cover;"></div>
+                <div class="carte-name">Ninja</div>
+                <div class="carte-desc">Stealth: At the start of your turn, give +1 HP to a random friendly minion</div>
+                <div class="carte-stats">
+                    <div class="carte-health">1</div>
+                    <div class="carte-atk">1</div>
+                </div>
+            </div>
+        </div>
+        <div class="my-misc">
+            <Button class="boutons-misc">Hero Power</Button>
+            <Button class="boutons-misc">Surrender</Button>
+            <div class="time-remaining">50</div>
+        </div>
     </div>
     
 </body>
